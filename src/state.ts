50b0fd278b16a6dcc07813bc4522749e049c6b17
lcha0138 <lcha0138@student.monash.edu>
2026-08-21 01:01:42 +0800

    implement Action interface and ToggleBitAt action

---
+++ src/state.ts
@@ -0,0 +1,29 @@
+import { Action, Bit, Constants, State } from "./type.ts";
+
+export const initialState: State = {
+    gameEnd: false,
+    bits: Array.from({ length: Constants.DIGIT_COUNT }, (): Bit => 0),
+    targets: [],
+    exit: [],
+    score: 0,
+    time: 0,
+    seed: Constants.INITIAL_SEED,
+    nextId: 0,
+};
+
+const flip = (b: Bit): Bit => (b === 0 ? 1 : 0);
+
+export class ToggleBitAt implements Action {
+    constructor(public readonly index: number) {}
+
+    apply(s: State): State {
+        return s.gameEnd || this.index < 0 || this.index >= s.bits.length
+            ? s
+            : {
+                  ...s,
+                  bits: s.bits.map((b, i) => (i === this.index ? flip(b) : b)),
+              };
+    }
+}
+
+export const reduceState = (s: State, action: Action): State => action.apply(s);
