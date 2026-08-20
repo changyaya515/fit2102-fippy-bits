cb6d7672bba011632664ac1c3bdfe5976973ac5f
lcha0138 <lcha0138@student.monash.edu>
2026-08-21 01:02:21 +0800

    define core types, initial game state, and constants ( some const are moved from main)

--- src/type.ts
+++ src/type.ts
@@ -1,4 +1,4 @@
-type Bit = 0 | 1;
+export type Bit = 0 | 1;
 
 export const Constants = {
     DIGIT_COUNT: 8,
@@ -6,54 +6,29 @@ export const Constants = {
     INITIAL_SEED: 1234,
 } as const;
 
-
-export type Target = Readonly<{
-    id: string;
-    value: number;
-    x: number;
-    y: number;
-    createTime: number;
-}>;
+export type FallingTarget = Readonly<{
+  id: string
+  value: number
+  x: number
+  y: number
+  createTime: number
+}>
 
 // State processing
 export type State = Readonly<{
     gameEnd: boolean;
     bits: ReadonlyArray<Bit>;
-    targets: ReadonlyArray<Target>;
-    exit: ReadonlyArray<Target>;
+    targets: ReadonlyArray<FallingTarget>;
+    exit: ReadonlyArray<FallingTarget>;
     score: number;
     time: number;
     seed: number;
     nextId: number;
 }>;
 
-const initialState: State = {
-    bits: Array.from({ length: Constants.DIGIT_COUNT }, (): Bit => 0),
-    targets: [],
-    exit: [],
-    score: 0,
-    time: 0,
-    seed: Constants.INITIAL_SEED,
-    nextId: 0,
-    gameEnd: false,
-};
 
 
 export interface Action {
     apply(s: State): State;
 }
 
-class ToggleBitAt implements Action {
-  constructor(public readonly index: number) {}
-
-  apply(s: State): State {
-    return s.gameEnd
-      ? s
-      : {
-          ...s,
-          bits: s.bits.map((bit, idx) =>
-            idx === this.index ? ((1 - bit) as Bit) : bit
-          ),
-        };
-  }
-}
