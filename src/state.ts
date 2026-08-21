f45e750d17e3bbb3f92386e18d2575f931a9a28e
lcha0138 <lcha0138@student.monash.edu>
2026-08-21 15:46:08 +0800

    Write RNG logic, which is learn from workshop4

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,26 @@
-import { Action, Bit, Constants, State } from "./type.ts";
+import { Action, Bit, Constants, State } from "./type";
+
+/*
+Cite from workshop 4
+*/
+export abstract class RNG {
+    private static m = 0x80000000; 
+    private static a = 1103515245;
+    private static c = 12345;
+
+    /**
+     * Call `hash` repeatedly to generate the sequence of hashes.
+     * @param seed 
+     * @returns a hash of the seed
+     */
+    public static hash = (seed: number) => (RNG.a * seed + RNG.c) % RNG.m;
+
+    /**
+     * Takes hash value and scales it to the range [-1, 1]
+     */
+    public static scale = (hash: number) => (2 * hash) / (RNG.m - 1) - 1;
+}
+
 
 export const initialState: State = {
     gameEnd: false,
